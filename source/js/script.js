920ea4b15b0443c1de2186c2d3ec4e39b6058e29
Ilya Gruzdev <gruzdevilyaa@gmail.com>
2023-04-08 13:21:55 +0300

    Коммит

---
+++ source/js/script.js
@@ -0,0 +1,24 @@
+let promoButton = document.querySelector('.promo__button');
+let sectionPets = document.querySelector('.pets');
+
+promoButton.addEventListener('click', smoothScroll);
+
+function smoothScroll() {
+    sectionPets.scrollIntoView({ behavior: 'smooth' });
+}
+
+let answersTextWrapper = Array.from(document.querySelectorAll('.answers__text-wrapper'));
+let answersList = document.querySelector('.answers__list');
+let answersButtons = Array.from(document.querySelectorAll('.answers__btn'));
+
+
+answersTextWrapper.forEach((item) => {
+  item.classList.add('answers__text-wrapper--closed');
+})
+
+answersButtons.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    answersTextWrapper[index].classList.toggle('answers__text-wrapper--closed');
+  })
+})
+
