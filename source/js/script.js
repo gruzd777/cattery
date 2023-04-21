c46e3c484129171c91c5ae8a0907607318448be5
Ilya Gruzdev <gruzdevilyaa@gmail.com>
2023-04-21 16:08:08 +0300

    Подготовлены скрипты

--- source/js/script.js
+++ source/js/script.js
@@ -10,13 +10,14 @@ function smoothScroll() {
 let answersTextWrapper = Array.from(document.querySelectorAll('.answers__text-wrapper'));
 let answersButtons = Array.from(document.querySelectorAll('.answers__btn'));
 
-
 answersTextWrapper.forEach((item) => {
   item.classList.add('answers__text-wrapper--closed');
 })
 
 answersButtons.forEach((item, index) => {
+  item.classList.add('answers__btn--closed');
   item.addEventListener('click', () => {
     answersTextWrapper[index].classList.toggle('answers__text-wrapper--closed');
+    item.classList.toggle('answers__btn--closed');
   })
 })
